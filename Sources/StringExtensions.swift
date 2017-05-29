9e3d0d2b6ee2cb81ba68ad39d118028ac45392f6
Dan Waltin <dan@waltin.se>
2017-05-29 07:53:12 +0200

    Added more string extensions

--- Sources/StringExtensions.swift
+++ Sources/StringExtensions.swift
@@ -1,6 +1,43 @@
 import Foundation
 
+internal let newLine = "\n"
+internal let tripleWhitespace = "   "
+internal let doubleWhitespace = "  "
+internal let singleWhitespace = " "
+
 public extension String {
+	
+	public func trim() -> String {
+		return self.trimmingCharacters(in: NSCharacterSet.whitespaces)
+	}
+	
+	public func appendLine() -> String {
+		return self + newLine
+	}
+	
+	public func appendLine(_ line: String) -> String {
+		if self == "" {
+			return self + line
+		}
+		return self + newLine + line
+	}
+	
+	public func compactWhitespace() -> String {
+		return _remove(tripleWhitespace)._remove(doubleWhitespace)
+	}
+	
+	private func _remove(_ whitespace: String) -> String {
+		if self.characters.count < whitespace.characters.count {
+			return self
+		}
+		
+		if !self.contains(whitespace) {
+			return self
+		}
+		
+		return self.replacingOccurrences(of: whitespace, with: singleWhitespace)
+	}
+	
 	public func camelCaseify() -> String {
 		let words = asNSString().components(separatedBy: NSCharacterSet.whitespaces)
 		
@@ -11,9 +48,29 @@ public extension String {
 		
 		return output
 	}
+	
+	public func allLines() -> [String] {
+		if self == "" {
+			return []
+		}
+		
+		return self.asNSString().components(separatedBy: newLine)
+	}
+	
+	public func deleteText(_ text: String) -> String {
+		return self.asNSString().replacingOccurrences(of: text, with: "")
+	}
 }
 
 extension String {
+	func pathExtension() -> String {
+		return asNSString().pathExtension
+	}
+	
+	func stringByDeletingPathExtension() -> String {
+		return asNSString().deletingPathExtension
+	}
+	
 	func asNSString() -> NSString {
 		return NSString(string: self)
 	}
--- Tests/DWExtensionsTests/StringExtensionsTests.swift
+++ Tests/DWExtensionsTests/StringExtensionsTests.swift
@@ -2,15 +2,56 @@ import XCTest
 @testable import DWExtensions
 
 class StringExtensionsTests: XCTestCase {
-	// MARK:
+	func test_trim() {
+		XCTAssertEqual(     "".trim(),  "")
+		XCTAssertEqual("line".trim(),   "line")
+		XCTAssertEqual("line ".trim(),  "line")
+		XCTAssertEqual(" line".trim(),  "line")
+		XCTAssertEqual(" line".trim(),  "line")
+		XCTAssertEqual(" line ".trim(), "line")
+	}
+	
+	func test_appendLine() {
+		XCTAssertEqual(""    .appendLine(""),     "")
+		XCTAssertEqual(""    .appendLine("line"), "line")
+		XCTAssertEqual("one" .appendLine("two"),  "one\ntwo")
+		XCTAssertEqual("line".appendLine(),       "line\n")
+	}
+	
+	func test_compactWhitespace() {
+		XCTAssertEqual(""         .compactWhitespace(), "")
+		XCTAssertEqual("  "       .compactWhitespace(), " ")
+		XCTAssertEqual("   "      .compactWhitespace(), " ")
+		XCTAssertEqual("one  "    .compactWhitespace(), "one ")
+		XCTAssertEqual("one two"  .compactWhitespace(), "one two")
+		XCTAssertEqual("one  two" .compactWhitespace(), "one two")
+		XCTAssertEqual("one   two".compactWhitespace(), "one two")
+		XCTAssertEqual("1  2   3" .compactWhitespace(), "1 2 3")
+	}
+	
 	func test_camelCaseify() {
-		XCTAssertEqual("",         "".camelCaseify())
+		XCTAssertEqual(""              .camelCaseify(), "")
 		
-		XCTAssertEqual("Word",     "word".camelCaseify())
-		XCTAssertEqual("Word",     "Word".camelCaseify())
-		XCTAssertEqual("Word",     "WORD".camelCaseify())
+		XCTAssertEqual("word"          .camelCaseify(), "Word")
+		XCTAssertEqual("Word"          .camelCaseify(), "Word")
+		XCTAssertEqual("WORD"          .camelCaseify(), "Word")
 		
-		XCTAssertEqual("OneTwo",      "one two".camelCaseify())
-		XCTAssertEqual("OneTwoThree", "one two  three".camelCaseify())
+		XCTAssertEqual("one two"       .camelCaseify(), "OneTwo")
+		XCTAssertEqual("one two  three".camelCaseify(), "OneTwoThree")
+	}
+	
+	func test_deleteText() {
+		XCTAssertEqual("Feature:title".deleteText("Feature:"), "title")
+	}
+	
+	func test_allLines() {
+		XCTAssertEqual(""        .allLines(), [])
+		XCTAssertEqual(" "       .allLines(), [" "])
+		XCTAssertEqual("line"    .allLines(), ["line"])
+		XCTAssertEqual("one two" .allLines(), ["one two"])
+		XCTAssertEqual("one\ntwo".allLines(), ["one", "two"])
+		XCTAssertEqual("1\n2\n3" .allLines(), ["1", "2", "3"])
+		XCTAssertEqual("\nline"  .allLines(), ["", "line"])
+		XCTAssertEqual("line\n"  .allLines(), ["line", ""])
 	}
 }
