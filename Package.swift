002c85055dc16107eb708a474247092a89236a36
Dan Waltin <dan@waltin.se>
2017-09-29 15:55:35 +0200

    Upgraded to swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "DWExtensions"
+	name: "DWExtensions",
+	targets: [
+		.target(name: "DWExtensions"),
+		.testTarget(name: "DWExtensionsTests", dependencies: ["DWExtensions"])]
 )
+
